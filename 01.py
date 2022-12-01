f038999525bafe55444b81a5bbecf93e355e38d7
Harry King <harry.king@me.com>
2022-12-01 12:17:45 +0100

    day 1

--- 01.py
+++ 01.py
@@ -4,4 +4,4 @@ elves = [map(int, elf.split()) for elf in stdin.read().split('\n\n')]
 elf_sums = list(map(sum, elves))
 
 print(max(elf_sums))
-print(sum(sorted(elf_sums, reverse=True)[:3]))
+print(sum(sorted(elf_sums)[-3:]))
