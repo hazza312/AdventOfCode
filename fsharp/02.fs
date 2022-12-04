0d434ad14e16aa5769d6abd560008fbfd084826a
harry <w.h.king@student.vu.nl>
2022-12-04 10:49:21 +0100

    refactor day2

--- fsharp/02.fs
+++ fsharp/02.fs
@@ -1,33 +1,28 @@
 open System
 
 type hand = Rock | Paper | Scissors
-type player_ending = Win | Draw | Lose
-type result = player_ending * hand
+type outcome = Win | Draw | Lose
 
 let to_hand = function "A" | "X" -> Rock | "B" | "Y" -> Paper | _ -> Scissors
 
-let loses_against = function 
-    Rock -> Scissors | Paper -> Rock | Scissors -> Paper
+let other_hand_for_outcome outcome h = match outcome with 
+| Lose -> (match h with Rock -> Scissors | Paper -> Rock | Scissors -> Paper)
+| Win -> (match h with Rock -> Paper | Paper -> Scissors | Scissors -> Rock)
+| Draw -> h
 
-let wins_against = function 
-    Rock -> Paper | Paper -> Scissors | Scissors -> Rock
-   
-let get_score (player_ending, hand): int = 
+let get_score (outcome, hand): int = 
     (match hand with Rock -> 1 | Paper -> 2 | Scissors -> 3)
-    + (match player_ending with Win -> 6 | Draw -> 3 | _ -> 0)
+    + (match outcome with Win -> 6 | Draw -> 3 | _ -> 0)
 
-let get_player_result = function  
-    | a, b when a = b -> Draw, b
-    | a, b when b = wins_against a -> Win, b
-    | _, b -> Lose, b
+let game_result (a', b') = 
+    let a, b = (to_hand a', to_hand b')
+    if a = b then Draw, b
+    else if other_hand_for_outcome Win a = b then Win, b
+    else Lose, b
 
-let part1 (a, b): result = 
-    (to_hand a, to_hand b) |> get_player_result
-
-let part2 = function
-    | a, "X" -> Lose, loses_against (to_hand a)
-    | a, "Y" -> Draw, to_hand a
-    | a, "Z" -> Win, wins_against (to_hand a)
+let forced_outcome (a, x) = 
+    let result = match x with "X" -> Lose | "Y" -> Draw | "Z" -> Win 
+    result, other_hand_for_outcome result (to_hand a)
 
 let rec read_lines () = 
     match Console.ReadLine() with null -> [] | s -> s :: read_lines ()
@@ -36,8 +31,7 @@ let rec read_lines () =
 let main =
     let split (line: string) = let [|a; b|] = line.Split " " in (a, b)
     let lines = read_lines () |> List.map split
-    let do_result parser = List.map (parser >> get_score) lines |> List.sum |> printfn "%d"
-
-    do_result part1  
-    do_result part2
-
+    let sum_scores f = List.fold (fun acc i -> acc + get_score (f i)) 0 lines
+    
+    sum_scores game_result  |> printfn "%d"
+    sum_scores forced_outcome |> printfn "%d"
