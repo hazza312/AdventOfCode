29dd913e7bc268b436b98a8d5638bd676eb59958
harry <w.h.king@student.vu.nl>
2022-12-04 15:13:04 +0100

    day4 c

--- c/04.c
+++ c/04.c
@@ -1,13 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define MAX(a,b)                        ((a > b) ? a : b)
+#define MIN(a,b)                        ((a < b) ? a : b)
+#define WITHIN(inl, inh, outl, outh)    (inl >= outl && inh <= outh)
+
 int main() {
     int lo1, hi1, lo2, hi2;
     int score1 = 0, score2 = 0;
     
-    while (scanf("%d-%d,%d-%d\n", &lo1, &hi1, &lo2, &hi2) > 0) {        
-        score1 += (lo1 >= lo2 && hi1 <= hi2) || (lo2 >= lo1 && hi2 <= hi1);
-        score2 += (lo1 >= lo2 && lo1 <= hi2) || (lo2 >= lo1 && lo2 <= hi1);             
+    while (scanf("%d-%d,%d-%d\n", &lo1, &hi1, &lo2, &hi2) > 0) {            
+        score1 += WITHIN(lo1, hi1, lo2, hi2) || WITHIN(lo2, hi2, lo1, hi1);
+        score2 += MAX(lo1, lo2) <= MIN(hi1, hi2);             
     }
     
     printf("%d\n%d\n", score1, score2);
