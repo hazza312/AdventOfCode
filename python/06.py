6249b5bf7f0ce0a9af9c3eb3cba2daccb967cf62
Harry King <harry.king@me.com>
2022-12-06 07:31:04 +0100

    Update 06.py

--- python/06.py
+++ python/06.py
@@ -1,9 +1,8 @@
 def find_marker(n, string):
-	for i in range(n, len(string)):
-		if len(set(string[i-n:i])) == n:
-			return i
+    for i in range(n, len(string)):
+        if len(set(string[i-n:i])) == n:
+            return i
 
 line = input()
 print(find_marker(4, line))
 print(find_marker(14, line))
- 
