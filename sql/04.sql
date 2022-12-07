bb51eecad78ad6694fb9c3dbe02ef914beb6a355
Harry <harry.king@me.com>
2022-12-07 21:43:31 +0100

    day4 sql

--- sql/04.sql
+++ sql/04.sql
@@ -2,19 +2,17 @@
 -- run with psql -f 04.sql 
 
 -- reset state
-DROP TABLE IF EXISTS day04_buf, day04_ranges;
-CREATE TABLE day04_buf (l VARCHAR(255), r VARCHAR(255));
-CREATE TABLE day04_ranges (l numrange, r numrange);
-
+DROP TABLE IF EXISTS day04_buf;
+CREATE TABLE day04_buf (l text, r text);
 \copy day04_buf (l, r) FROM 'input4.txt' WITH DELIMITER ',' CSV;
 
--- parse input as ranges
-INSERT INTO day04_ranges (l,r)
-SELECT  ('[' || REPLACE(l, '-', ',') || ']')::numrange AS l, 
-        ('[' || REPLACE(r, '-', ',') || ']')::numrange AS r
-FROM    day04_buf;
-    
--- query
+-- parse & query
+WITH ranges AS (
+    SELECT  ('[' || REPLACE(l, '-', ',') || ']')::numrange AS l, 
+            ('[' || REPLACE(r, '-', ',') || ']')::numrange AS r
+    FROM    day04_buf
+)   
+
 SELECT  SUM((l @> r OR r @> l)::int) AS part1,
         SUM((l && r)::int) AS part2
-FROM    day04_ranges;   
+FROM    ranges;   
