f66dee30f4c9e528f595d8324bf01ffcda2e9dda
Harry <harry.king@me.com>
2022-12-09 09:30:44 +0100

    day9 python

--- python/09.py
+++ python/09.py
@@ -11,16 +11,11 @@ MOVE_THRESHOLD = abs(xy(1, 1))
 def signum(x):
     return 0 if x == 0 else x / abs(x)
 
-def offset_signum(a, b):
-    x1, y1 = a.real, a.imag
-    x2, y2 = b.real, b.imag
-    return xy(signum(x2 - x1), signum(y2 - y1))
-
-def next_offset(tail, head):
-    if abs(head - tail) <= MOVE_THRESHOLD:
+def next_offset(offset):
+    if abs(offset) <= MOVE_THRESHOLD:
         return xy(0, 0)
     
-    return offset_signum(tail, head)
+    return xy(signum(offset.real), signum(offset.imag))
     
 def num_tail_positions(commands, rope_length):
     rope = [xy(0, 0)] * rope_length
@@ -30,7 +25,7 @@ def num_tail_positions(commands, rope_length):
         for _ in range(int(n)):
             rope[0] += OFFSETS[direction]
             for i in range(1, len(rope)):
-                rope[i] += next_offset(rope[i], rope[i-1])
+                rope[i] += next_offset(rope[i-1] - rope[i])
 
             tail_visited.add(rope[-1])
                 
